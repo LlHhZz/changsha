e21d9c113ef3b1d48be4326b73efc8f78511ab1c
LlHhZz <2621015386@qq.com>
2024-10-01 22:23:39 +0800

    update winning result model

--- src/components/declarationResults/ElectricalEnergyWinningResults.vue
+++ src/components/declarationResults/ElectricalEnergyWinningResults.vue
@@ -1,76 +1,78 @@
 <template>
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <!-- <th scope="col">运行时间</th> -->
-                <th scope="col">中标类型</th>
-                <th scope="col">价格</th>
-                <!-- <th scope="col">光伏出力价格</th> -->
-                <th scope="col">开始时间</th>
-                <th scope="col">结束时间</th>
-            </tr>
-        </thead>
-        <tbody v-if="P_dis.length">
-            <tr>
-                <td>储能放电</td>
-                <td>{{P_dis[0]}}</td>
-                <!-- <td>{{P_pv[0]}}</td> -->
-                <td>2024-08-22 00:00:00</td>
-                <td>2022-08-22 24:00:00</td>
-            </tr>
-            <tr>
-                <td>光伏出力</td>
-                <td>{{P_pv[0]}}</td>
-                <!-- <td>XX</td> -->
-                <td>2024-08-23 00:00:00</td>
-                <td>2024-08-23 24:00:00</td>
-            </tr>
-            <tr>
-                <td>储能放电</td>
-                <td>{{P_dis[1]}}</td>
-                <!-- <td>{{P_pv[0]}}</td> -->
-                <td>2024-08-24 00:00:00</td>
-                <td>2022-08-24 24:00:00</td>
-            </tr>
-            <tr>
-                <td>光伏出力</td>
-                <td>{{P_pv[1]}}</td>
-                <!-- <td>XX</td> -->
-                <td>2024-08-25 00:00:00</td>
-                <td>2024-08-25 24:00:00</td>
-            </tr>
-            <tr>
-                <td>储能放电</td>
-                <td>{{P_pv[2]}}</td>
-                <!-- <td>XX</td> -->
-                <td>2024-08-26 00:00:00</td>
-                <td>2024-08-26 24:00:00</td>
-            </tr>
-            <tr>
-                <td>光伏出力</td>
-                <td>{{P_pv[2]}}</td>
-                <!-- <td>XX</td> -->
-                <td>2024-08-27 00:00:00</td>
-                <td>2024-08-27 24:00:00</td>
-            </tr>
-        </tbody>
+    <div class="table-container">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <!-- <th scope="col">运行时间</th> -->
+                    <th scope="col">中标类型</th>
+                    <th scope="col">价格</th>
+                    <!-- <th scope="col">光伏出力价格</th> -->
+                    <th scope="col">开始时间</th>
+                    <th scope="col">结束时间</th>
+                </tr>
+            </thead>
+            <tbody v-if="P_dis.length">
+                <tr>
+                    <td>储能放电</td>
+                    <td>{{P_dis[0]}}</td>
+                    <!-- <td>{{P_pv[0]}}</td> -->
+                    <td>2024-08-22 00:00:00</td>
+                    <td>2022-08-22 24:00:00</td>
+                </tr>
+                <tr>
+                    <td>光伏出力</td>
+                    <td>{{P_pv[0]}}</td>
+                    <!-- <td>XX</td> -->
+                    <td>2024-08-23 00:00:00</td>
+                    <td>2024-08-23 24:00:00</td>
+                </tr>
+                <tr>
+                    <td>储能放电</td>
+                    <td>{{P_dis[1]}}</td>
+                    <!-- <td>{{P_pv[0]}}</td> -->
+                    <td>2024-08-24 00:00:00</td>
+                    <td>2022-08-24 24:00:00</td>
+                </tr>
+                <tr>
+                    <td>光伏出力</td>
+                    <td>{{P_pv[1]}}</td>
+                    <!-- <td>XX</td> -->
+                    <td>2024-08-25 00:00:00</td>
+                    <td>2024-08-25 24:00:00</td>
+                </tr>
+                <tr>
+                    <td>储能放电</td>
+                    <td>{{P_pv[2]}}</td>
+                    <!-- <td>XX</td> -->
+                    <td>2024-08-26 00:00:00</td>
+                    <td>2024-08-26 24:00:00</td>
+                </tr>
+                <tr>
+                    <td>光伏出力</td>
+                    <td>{{P_pv[2]}}</td>
+                    <!-- <td>XX</td> -->
+                    <td>2024-08-27 00:00:00</td>
+                    <td>2024-08-27 24:00:00</td>
+                </tr>
+            </tbody>
 
-        <!--
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th v-for="idx in time" :key="idx">{{ idx }}:00</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item_i, idx_i) in data" :key="idx_i">
-                <td>{{ idx_i + 1 }}</td>
-                <td v-for="(item_j, idx_j) in item_i" :key="idx_j">{{ item_j }}</td>
-            </tr>
-        </tbody>
-        -->
-        
-    </table>
+            <!--
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th v-for="idx in time" :key="idx">{{ idx }}:00</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item_i, idx_i) in data" :key="idx_i">
+                    <td>{{ idx_i + 1 }}</td>
+                    <td v-for="(item_j, idx_j) in item_i" :key="idx_j">{{ item_j }}</td>
+                </tr>
+            </tbody>
+            -->
+            
+        </table>
+    </div>
 </template>
 
 <script>
@@ -130,6 +132,12 @@ export default {
 </script>
 
 <style scoped>
+.table-container {
+  width: 100%; /* 容器宽度 */
+  height: 100%; /* 容器高度 */
+  overflow: hidden; /* 隐藏超出容器大小的内容 */
+}
+
 .table {
   font-size: 12px;
   background-color: rgba(255, 255, 255, 0.08);
